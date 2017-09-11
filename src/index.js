7f069465b648691c9898d8bf5150c494ca9dce56
vamsi krishna <vamc.koduri@gmail.com>
2017-09-11 10:37:00 +0530

    module 2 ajax

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React,{ Component} from 'react';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY="AIzaSyDdJEnCJLK6jLKnCIyMKOxyZeMk_xU-ubQ";
 
 
-const App = () =>
+class App extends Component
 {
-    return <div>updated value </div>
+	constructor(props){
+	super(props);
+	
+	this.state = { 
+		videos : [],
+		selectedVideo : null }; 
+
+		this.vidoeSearch("vamsi")
+	
+	}
+
+
+	vidoeSearch(term)
+	{
+		debugger;
+		YTSearch( {key: API_KEY, term: term}, (videos) => {
+		this.setState( {
+		 videos : videos,
+		 selectedVideo : videos[0]
+
+		} )
+
+	});
+	}
+	
+
+
+	render()
+	{
+
+	const videoSearch = _.debounce( (term) => {this.vidoeSearch(term)},300);
+
+	return (
+    	<div>
+    		<SearchBar  onSearchTermChange = { videoSearch }/>
+    		<VideoDetail video={this.state.selectedVideo} />
+    		<VideoList   videos={this.state.videos} onVideoSelect = {selectedVideo => this.setState( {selectedVideo}) } />
+
+    	</div>
+    	)	
+	}
+    
 }
 
 
